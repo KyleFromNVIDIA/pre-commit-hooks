e15bf7f56bc74348a3dab8f0a7da5289e89d1e14
Kyle Edwards <kyedwards@nvidia.com>
2026-08-05 14:57:33 -0400

    Handle new constraints output type

--- src/rapids_pre_commit_hooks/dependencies/cuda_suffixed.py
+++ src/rapids_pre_commit_hooks/dependencies/cuda_suffixed.py
@@ -94,7 +94,7 @@ class CUDASuffixedHandler(Handler):
         ),
         item: "yaml.Node",
     ) -> None:
-        if item.value in {"requirements", "pyproject"}:
+        if item.value in {"requirements", "constraints", "pyproject"}:
             output_types_context.has_python_output_type = True
 
     @contextlib.contextmanager
